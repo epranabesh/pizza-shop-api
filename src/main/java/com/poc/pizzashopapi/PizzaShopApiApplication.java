7add3181d6b1d46c60e34fb71fe177f5467c2713
Handa, Pranabesh <HANDAP1@nationwide.com>
2018-10-17 10:54:50 -0400

    test

---
+++ src/main/java/com/poc/pizzashopapi/PizzaShopApiApplication.java
@@ -0,0 +1,12 @@
+package com.poc.pizzashopapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaShopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaShopApiApplication.class, args);
+	}
+}
