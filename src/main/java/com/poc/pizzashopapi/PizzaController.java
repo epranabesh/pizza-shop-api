802278936102859aa9d182cd1385f59cfed0df8f
Handa, Pranabesh <HANDAP1@nationwide.com>
2018-10-17 11:51:00 -0400

    update display

--- src/main/java/com/poc/pizzashopapi/PizzaController.java
+++ src/main/java/com/poc/pizzashopapi/PizzaController.java
@@ -8,6 +8,6 @@ public class PizzaController {
 
     @RequestMapping("/pizzas")
     public String getPizzas() {
-        return "Pizza is good";
+        return "Pizza was good but better now";
     }
 }
